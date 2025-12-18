0ec52d007fd50f75f1f0c130ce2ef83768391b8d
tfengaka <nthoa2.lhp.nh@gmail.com>
2025-12-18 15:41:00 +0700

    init

---
+++ src/content/config.ts
@@ -0,0 +1,27 @@
+import { defineCollection, z } from "astro:content";
+
+const experience = defineCollection({
+	type: "data",
+	schema: z.object({
+		company: z.string(),
+		role: z.string(),
+		startDate: z.string(),
+		endDate: z.string(),
+		bullets: z.array(z.string()),
+	}),
+});
+
+const projects = defineCollection({
+	type: "data",
+	schema: z.object({
+		name: z.string(),
+		description: z.string(),
+		techStack: z.array(z.string()),
+		link: z.string().optional(),
+	}),
+});
+
+export const collections = {
+	experience,
+	projects,
+};
