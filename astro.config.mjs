dded511840aab0c4318571e6111210cdcd917133
tfengaka <nthoa2.lhp.nh@gmail.com>
2025-12-18 15:47:19 +0700

    setup deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
+	vite: { plugins: [tailwindcss()] },
+	site: "https://tfengaka.github.io",
+	integrations: [react()],
 });
